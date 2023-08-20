6df25f3b8ad528984a6b197594b1899b96c8c089
Alina Lipova <alina.ag@gmail.com>
2023-08-20 16:24:54 +0500

    Fixed faker deprecated method Added coverage

--- framework/config/config.js
+++ framework/config/config.js
@@ -1,14 +1,14 @@
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
 const config = {
-    url:"https://bookstore.demoqa.com",
-    credentials: {
-        'userName': faker.internet.userName(),
-        'password': faker.internet.password(20, false, /[a-zA-Z0-9_$%#!@]/, '_')
-    },
-    credentialsStatic: {
-        'password': "rwA234w_e3#22e"
-    },
+  url: 'https://bookstore.demoqa.com',
+  credentials: {
+    userName: faker.internet.userName(),
+    password: faker.internet.password({ length: 20, memorable: false, pattern: /[a-zA-Z0-9_$%#!@]/, prefix: '_' })
+  },
+  credentialsStatic: {
+    password: 'rwA234w_e3#22e'
+  }
 
 }
 export default config
